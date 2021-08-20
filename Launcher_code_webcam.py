e9930e96a16577eb88fffbffa175672372261088
Jun Song <thdwlgnsdlek@mail.ulsan.ac.kr>
2021-08-20 12:06:12 +0900

    Update Launcher_code_webcam.py

--- Launcher_code_webcam.py
+++ Launcher_code_webcam.py
@@ -78,8 +78,9 @@ parser.add_argument('--labels', help='Name of the labelmap file, if different th
                     default='labelmap.txt')
 parser.add_argument('--threshold', help='Minimum confidence threshold for displaying detected objects',
                     default=0.5)
+##Revise resolution
 parser.add_argument('--resolution', help='Desired webcam resolution in WxH. If the webcam does not support the resolution entered, errors may occur.',
-                    default='1280x720')
+                    default='640x480')
 parser.add_argument('--edgetpu', help='Use Coral Edge TPU Accelerator to speed up detection',
                     action='store_true')
 
