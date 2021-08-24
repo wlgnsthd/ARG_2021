ed4ab6f8098a65e88e5c73030ad8359fd5fbc0e8
Jun Song <thdwlgnsdlek@mail.ulsan.ac.kr>
2021-08-24 13:27:03 +0900

    Update Launcher_code_webcam.py

--- Launcher_code_webcam.py
+++ Launcher_code_webcam.py
@@ -30,7 +30,7 @@ servo.min()
 # Source - Adrian Rosebrock, PyImageSearch: https://www.pyimagesearch.com/2015/12/28/increasing-raspberry-pi-fps-with-python-and-opencv/
 class VideoStream:
     """Camera object that controls video streaming from the Picamera"""
-    def __init__(self,resolution=(640,480),framerate=30):
+    def __init__(self,resolution=(320,240),framerate=60):
         # Initialize the PiCamera and the camera image stream
         self.stream = cv2.VideoCapture(0)
         ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
@@ -80,7 +80,7 @@ parser.add_argument('--threshold', help='Minimum confidence threshold for displa
                     default=0.5)
 ##Revise resolution
 parser.add_argument('--resolution', help='Desired webcam resolution in WxH. If the webcam does not support the resolution entered, errors may occur.',
-                    default='640x480')
+                    default='320x240')
 parser.add_argument('--edgetpu', help='Use Coral Edge TPU Accelerator to speed up detection',
                     action='store_true')
 
