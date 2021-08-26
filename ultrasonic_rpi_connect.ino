9c6bdf5038fd538e0be0f8726b02a26f552e1f3e
Jun Song <thdwlgnsdlek@mail.ulsan.ac.kr>
2021-08-26 22:39:05 +0900

    Update ultrasonic_rpi_connect.ino

--- ultrasonic_rpi_connect.ino
+++ ultrasonic_rpi_connect.ino
@@ -9,7 +9,7 @@ int angle = 0;
 int active_angle = 90;
 
 int values[5];//[x각도3,y각도3,속도2,안전모드해제1,고도2]
-
+float realvalues[5];
 void setup()
 {
    Serial.begin(9600);    
@@ -23,7 +23,7 @@ void loop()
 {  //if (Serial.available() > 0){
 //라즈베리파이에서 안전모드해제(1,2)/x축각도(0~90.0)/y축각도(0~90.0)/속도(0~9.0)/ [45.0/45.0/9.0/0]9자리
   //String data = Serial.readStringUntil('\n');
-  String data = "123456789";
+  String data = "145045068";
   long int main_value = data.toInt();
   Serial.println(main_value);
 
@@ -31,11 +31,15 @@ void loop()
   values[0] = main_value/100000000; //안전모드해제1
   values[1] = main_value/100000-values[0]*1000; //x축각도450
   values[2] = main_value/100-(values[0]*1000000+values[1]*1000); //y축각도450
-  values[3] = main_value-(values[0]*100000000+values[1]*100000+values[2]*100); //속도68
-  Serial.println(values[0]);
-  Serial.println(values[1]);
- Serial.println(values[2]);
- Serial.println(values[3]);
+  values[3] = main_value/1-(values[0]*100000000+values[1]*100000+values[2]*100); //속도68   
+  realvalues[0] = values[0];
+   realvalues[1] = values[1]*0.1;
+    realvalues[2] = values[2]*0.1;
+     realvalues[3] = values[3]*0.1;
+  Serial.println(realvalues[0]);
+  Serial.println(realvalues[1]);
+ Serial.println(realvalues[2]);
+ Serial.println(realvalues[3]);
 
  
  
@@ -56,7 +60,7 @@ void loop()
     Serial.println("m");
     //Serial.println(values[0]);
 //서보 작동해서 투하
-    if (values[0]==123){
+    if (values[0]==1){
         servo.write(active_angle); 
         delay(500);
         servo.write(angle);
