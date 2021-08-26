b08fa009cdf4a9e4d902a36941169307458c8f8b
Jun Song <thdwlgnsdlek@mail.ulsan.ac.kr>
2021-08-26 22:27:26 +0900

    Update ultrasonic_rpi_connect.ino

--- ultrasonic_rpi_connect.ino
+++ ultrasonic_rpi_connect.ino
@@ -2,12 +2,13 @@
 #define echo 9
 #include <Servo.h> 
 
+
 Servo servo; 
 int servoPin = 10;
 int angle = 0; 
 int active_angle = 90;
 
-float values[5];//[x각도3,y각도3,속도2,안전모드해제1,고도2]
+int values[5];//[x각도3,y각도3,속도2,안전모드해제1,고도2]
 
 void setup()
 {
@@ -19,16 +20,25 @@ void setup()
 
 
 void loop()
-{  if (Serial.available() > 0){
-//라즈베리파이에서 x축각도(0~90.0)/y축각도(0~90.0)/속도(0~9.0)/안전모드해제(0,1) [45.0/45.0/9.0/0]9자리
-    String data = Serial.readStringUntil('\n');
-    Serial.print("From rpi : ");
-    Serial.println(data);
-//받은 데이터 분류 csv   
-    values[0] = data[0]+data[1]+data[2]; //x각도
-    values[1] = data[3]+data[4]+data[5]; //y각도
-    values[2] = data[6]+data[7]; //속도
-    values[3] = data[8] //안전모드해제
+{  //if (Serial.available() > 0){
+//라즈베리파이에서 안전모드해제(1,2)/x축각도(0~90.0)/y축각도(0~90.0)/속도(0~9.0)/ [45.0/45.0/9.0/0]9자리
+  //String data = Serial.readStringUntil('\n');
+  String data = "123456789";
+  long int main_value = data.toInt();
+  Serial.println(main_value);
+
+//string data에서 int변수로 바꾸기   
+  values[0] = main_value/100000000; //안전모드해제1
+  values[1] = main_value/100000-values[0]*1000; //x축각도450
+  values[2] = main_value/100-(values[0]*1000000+values[1]*1000); //y축각도450
+  values[3] = main_value-(values[0]*100000000+values[1]*100000+values[2]*100); //속도68
+  Serial.println(values[0]);
+  Serial.println(values[1]);
+ Serial.println(values[2]);
+ Serial.println(values[3]);
+
+ 
+ 
 //초음파  
     float duration, distance;    
     digitalWrite(trig, LOW);        
@@ -44,7 +54,7 @@ void loop()
     Serial.print("distance: ");
     Serial.print(distance); 
     Serial.println("m");
-
+    //Serial.println(values[0]);
 //서보 작동해서 투하
     if (values[0]==123){
         servo.write(active_angle); 
@@ -52,5 +62,5 @@ void loop()
         servo.write(angle);
         delay(500);
    }
- } 
+// } 
 }
