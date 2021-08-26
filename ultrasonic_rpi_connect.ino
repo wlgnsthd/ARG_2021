afb8cfa0520fa6780036de9eab18c41e8ddb0645
Jun Song <thdwlgnsdlek@mail.ulsan.ac.kr>
2021-08-26 19:45:57 +0900

    Update ultrasonic_rpi_connect.ino

--- ultrasonic_rpi_connect.ino
+++ ultrasonic_rpi_connect.ino
@@ -7,7 +7,7 @@ int servoPin = 10;
 int angle = 0; 
 int active_angle = 90;
 
-float values[4];//[각도3,속도2,안전모드해제1,고도2]
+float values[5];//[x각도3,y각도3,속도2,안전모드해제1,고도2]
 
 void setup()
 {
@@ -20,13 +20,15 @@ void setup()
 
 void loop()
 {  if (Serial.available() > 0){
-//라즈베리파이에서 각도(0~90.0) 속도(0~9.0) 안전모드해제(0,1) 읽어오기 "56.5/9.5/0":6자리
+//라즈베리파이에서 x축각도(0~90.0)/y축각도(0~90.0)/속도(0~9.0)/안전모드해제(0,1) [45.0/45.0/9.0/0]9자리
     String data = Serial.readStringUntil('\n');
     Serial.print("From rpi : ");
     Serial.println(data);
 //받은 데이터 분류 csv   
-    values[0] = data[0:2]
-    values[0] = data[0:2]
+    values[0] = data[0]+data[1]+data[2]; //x각도
+    values[1] = data[3]+data[4]+data[5]; //y각도
+    values[2] = data[6]+data[7]; //속도
+    values[3] = data[8] //안전모드해제
 //초음파  
     float duration, distance;    
     digitalWrite(trig, LOW);        
@@ -38,13 +40,13 @@ void loop()
     duration = pulseIn(echo, HIGH);  
  
     distance = duration * 0.000170;
- 
+    values[4] = distance; //고도
     Serial.print("distance: ");
     Serial.print(distance); 
     Serial.println("m");
 
 //서보 작동해서 투하
-    if (){
+    if (values[0]==123){
         servo.write(active_angle); 
         delay(500);
         servo.write(angle);
