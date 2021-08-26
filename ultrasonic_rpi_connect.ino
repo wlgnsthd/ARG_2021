5691614f6ecfe5eb3444d8958bf58c8a61c66be2
Jun Song <thdwlgnsdlek@mail.ulsan.ac.kr>
2021-08-26 18:46:36 +0900

    Update ultrasonic_rpi_connect.ino

--- ultrasonic_rpi_connect.ino
+++ ultrasonic_rpi_connect.ino
@@ -7,6 +7,7 @@ int servoPin = 10;
 int angle = 0; 
 int active_angle = 90;
 
+float values[4];//[각도3,속도2,안전모드해제1,고도2]
 
 void setup()
 {
@@ -19,14 +20,15 @@ void setup()
 
 void loop()
 {  if (Serial.available() > 0){
-//라즈베리파이에서 각도(0~90.0) 속도(0~9.0) 안전모드해제(0,1) 읽어오기 "56.5,9.5,0"
+//라즈베리파이에서 각도(0~90.0) 속도(0~9.0) 안전모드해제(0,1) 읽어오기 "56.5/9.5/0":6자리
     String data = Serial.readStringUntil('\n');
     Serial.print("From rpi : ");
     Serial.println(data);
-//받은 데이터 분류 csv   문자열처리
-
+//받은 데이터 분류 csv   
+    values[0] = data[0:2]
+    values[0] = data[0:2]
 //초음파  
-    long duration, distance;    
+    float duration, distance;    
     digitalWrite(trig, LOW);        
     delayMicroseconds(2);            
     digitalWrite(trig, HIGH);   
@@ -40,12 +42,13 @@ void loop()
     Serial.print("distance: ");
     Serial.print(distance); 
     Serial.println("m");
-//쏠지말지 계산 
 
-//    if {
+//서보 작동해서 투하
+    if (){
         servo.write(active_angle); 
-        delay(5000)
+        delay(500);
         servo.write(angle);
-//    }
- }
+        delay(500);
+   }
+ } 
 }
