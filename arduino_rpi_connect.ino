cb60fa7e2203a86f139e697b67f0b2a1a40ab5f3
Jun Song <thdwlgnsdlek@mail.ulsan.ac.kr>
2021-08-29 15:26:27 +0900

    Update arduino_rpi_connect.ino

--- arduino_rpi_connect.ino
+++ arduino_rpi_connect.ino
@@ -2,17 +2,20 @@
 #define echo 9
 #include <Servo.h> 
 #include <math.h>
-
+#include <stdlib.h>
 
 Servo servo; 
 const int servoPin = 10;
-int angle = 0; 
-int active_angle = 90;
+const int angle = 0; 
+const int active_angle = 90;
 
 const int buzzer = 3;
 const int receiver_pin = 5;
-int values[4];//[x각도3,y각도3,속도2,고도2]
-float realvalues[4];
+int values[3]; 
+
+int i,safe_mode;
+float duration, distance, distance_test, cond1, cond2, cond3; 
+
 void setup()
 {
    Serial.begin(9600);    
@@ -25,67 +28,78 @@ void setup()
 
 
 void loop()
-{  //if (Serial.available() > 0){
-//라즈베리파이에서 /x축각도(0~90.0)/y축각도(0~90.0)/속도(0~9.0)/ [45.0/45.0/9.0/0]9자리
+{ //initialize
+  i =0;
+  //read data sent by rpi(|x angle*10|/y angle*10/velocity*10)
   //String data = Serial.readStringUntil('\n');
-  String data = "45045068";
-   //string data에서 int변수로 변환
-  long int main_value = data.toInt();
-  values[0] = main_value/100000; //x축각도450 deg
-  values[1] = main_value/100-values[0]*1000; //y축각도450 deg
-  values[2] = main_value-(values[0]*100000+values[1]*100); //속도 6.8
-  realvalues[0] = values[0]*0.001745; //x축각도 rad
-   realvalues[1] = values[1]*0.001745; // y축각도 rad
-    realvalues[2] = values[2]*0.1; //속도m/s
-   float cond1 = atan(realvalues[2]*sqrt(0.20408*realvalues[3])*0.66667);
-   float cond2 = atan((realvalues[2]*sqrt(0.20408*realvalues[3])-4.5)/realvalues[3])-0.78540;
-   float cond3 = atan((realvalues[2]*sqrt(0.20408*realvalues[3])+4.5)/realvalues[3])-0.78540;
+  String data = "468,-789,68";
+  //string data to int
+  char data_char[20]; //length of data
+  data.toCharArray(data_char,20);
+  char *ptr = strtok(data_char, ","); //classify with ","
+  while (ptr != NULL)              
+    {
+        values[i] = atoi(ptr);
+        ptr = strtok(NULL, ",");      
+        i = i+1;
+    }
+    
+   //ultrasonic_height 
+  digitalWrite(trig, LOW);        
+  delayMicroseconds(2);            
+  digitalWrite(trig, HIGH);   
+  delayMicroseconds(10);            
+  digitalWrite(trig, LOW);        
+   
+  duration = pulseIn(echo, HIGH);  
+  //height filtering
+  distance_test = duration * 0.000170;
+  if(distance_test >= 2.0 && distance_test <= 8.0)
+  {
+  distance = distance_test; //reliable height value
+  }
+  else
+  {
+  Serial.println("unreliable ultrasonic sensor value occured");      
+  }
+  //deploy condition
+  cond1 = atan((float(values[2])*0.1)*sqrt(0.20408*distance)*0.66667);
+  cond2 = atan(((float(values[2])*0.1)*sqrt(0.20408*distance)-4.5)/distance)-0.78540;
+  cond3 = atan(((float(values[2])*0.1)*sqrt(0.20408*distance)+4.5)/distance)-0.78540;
 
- //안전모드 확인
-   int safe_mode = pulseIn(receiver_pin,HIGH);
- 
-//초음파 고도 
-    float duration, distance;    
-    digitalWrite(trig, LOW);        
-    delayMicroseconds(2);            
-    digitalWrite(trig, HIGH);   
-    delayMicroseconds(10);            
-    digitalWrite(trig, LOW);        
- 
-    duration = pulseIn(echo, HIGH);  
- 
-    distance = duration * 0.000170;
-    realvalues[3] = distance; //고도 m
-    Serial.print("distance: ");
-    Serial.print(distance); 
-    Serial.println("m");
-Serial.print("realvalues[0]x각도 : ");    
-Serial.println(realvalues[0]);
-Serial.print("realvalues[1]y각도 : ");
-Serial.println(realvalues[1]);
-Serial.print("realvalues[2]속도 : ");
-Serial.println(realvalues[2]);
-Serial.print("realvalues[3]고도 : ");
-Serial.println(realvalues[3]);
-Serial.println("----condition----");
-Serial.print("x각도   오차 : ");
-Serial.println(cond1);
-Serial.print("y각도 최소값 : ");
-Serial.println(cond2);              
-Serial.print("y각도 최대값 : ");          
-Serial.println(cond3);                       
+ //verify safemode
+  safe_mode = pulseIn(receiver_pin,HIGH);
+  
+  Serial.println("----VALUES----");
+  Serial.print("SAFE_MODE : ");    
+  Serial.println(safe_mode);
+  Serial.print("|X_ANGLE| : ");    
+  Serial.println(values[0]*0.1);
+  Serial.print("Y_ANGLE : ");
+  Serial.println(values[1]*0.1);
+  Serial.print("VELOCITY : ");
+  Serial.println(values[2]*0.1);
+  Serial.print("HEIGHT : ");
+  Serial.println(distance);
+  Serial.println("----CONDITION----");
+  Serial.print("X_ANGLE COND : ");
+  Serial.println(cond1);
+  Serial.print("Y_ANGLE MIN: ");
+  Serial.println(cond2);              
+  Serial.print("Y_ANGLE MAX: ");          
+  Serial.println(cond3);      
+  Serial.println("----------------");                 
 
-//서보 작동해서 투하
-  //if(safe_mode >= 1500){ //값 확인!
-    if (realvalues[0]<=cond1){ //투하조건2
-     tone(buzzer, 1000);
-     if (realvalues[1]>= cond2 && realvalues[1]<=cond3){ //투하조건1
-        //servo.write(active_angle); 
-        //delay(2000);
-        //servo.write(angle);
-        //delay(500);
-     }
-    } 
+//deploy
+  //if(safe_mode >= 1500){ //from rc receiver
+    if ((float(values[0])*0.001745)<=cond1){ 
+      tone(buzzer, 1000);
+        if ((float(values[1])*0.001745)>= cond2 && (float(values[1])*0.001745)<=cond3){ 
+          servo.write(active_angle); 
+          delay(2000);
+          servo.write(angle);
+          delay(500);
+          }
+      } 
    //}
-//available } 
 }
